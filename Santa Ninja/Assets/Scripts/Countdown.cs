4d3b0d750aafd6d048b5d47cc2c55d108ace674e
Marie <m.skola>
2018-12-20 18:27:30 +0100

    countdown

---
+++ Santa Ninja/Assets/Scripts/Countdown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Countdown : MonoBehaviour
+{
+    public int timeLeft = 60;
+    public Text countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown.text = ("" + timeLeft);
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}

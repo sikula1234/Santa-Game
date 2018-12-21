49f21e820ef1c6559b441ef195845f62754f5f21
riskinDesktop <hlavac.honza@email.cz>
2018-12-21 18:22:33 +0100

    Pokladani darku - zobrazi se darek, nefunguje zmena poctu darku

---
+++ Santa Ninja/Assets/OdevzdaniDarku.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OdevzdaniDarku : MonoBehaviour
+{
+
+    public bool jeUzDarek = false;
+    public int pocetDarku = 4;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && jeUzDarek == false)
+        {
+            var image = GetComponent<SpriteRenderer>();
+            var tempColor = image.color;
+            tempColor.a = 1f;
+            image.color = tempColor;
+
+            Darky darky = FindObjectOfType<Darky>();
+            darky.actualNumberOfGifts = darky.actualNumberOfGifts - 1;
+
+            jeUzDarek = true;
+        }
+    }
+}

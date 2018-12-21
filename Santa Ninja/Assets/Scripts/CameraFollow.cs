bb909f401d75ebcea355a9e39c8f20ebb5cbb862
Morcinus <30431494+Morcinus@users.noreply.github.com>
2018-12-21 20:45:44 +0100

    Radoby Opraveni Pohybu kamery

---
+++ Santa Ninja/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public Transform target;
+	public float movementSpeed = 0.1f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (target)
+		{
+			transform.position = Vector3.Lerp(transform.position, target.position, movementSpeed) + new Vector3(0, 0, -10);
+		}
+
+	}
+}

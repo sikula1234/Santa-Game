860b436770dfc42bda6673fbaf2b04be4edcc983
riskinDesktop <hlavac.honza@email.cz>
2018-12-22 19:50:41 +0100

    MobSpawner zapocat

---
+++ Santa Ninja/Assets/MobSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject[] mobPrefabs;
+    public GameObject[] mobs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mobs = new GameObject[10]; //makes sure they match length
+        for (int i = 0; i < mobs.Length; i++)
+        {
+            int rand = Random.Range(0, mobPrefabs.Length - 1);
+            mobs[i] = Instantiate(mobPrefabs[rand]) as GameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

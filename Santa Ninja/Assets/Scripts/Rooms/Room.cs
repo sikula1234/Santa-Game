3b34fca87fa3f964e34824931c43531b74eafc88
Morcinus <30431494+Morcinus@users.noreply.github.com>
2018-12-23 21:08:23 +0100

    Added Level Generator

--- Santa Ninja/Assets/Scripts/Rooms/Room.cs
+++ Santa Ninja/Assets/Scripts/Rooms/Room.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Room : MonoBehaviour
 {
+	//Souradky pro generator
+	public bool isDiscovered = false;
+	public Vector2 position;
+
+	public Tilemap tilemap;
+
 	enum roomType
 	{
 		diningRoom, bathRoom, livingRoom, hall, kitchen
@@ -23,8 +30,13 @@ public class Room : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		//Pro Testovani - pak smazat
+		if (!isDiscovered)
+		{
+			tilemap.color = new Color(0, 0, 0);
+		} else tilemap.color = new Color(255, 255, 255);
+
+	}
 
 	public static void GenerateRoom()
 	{

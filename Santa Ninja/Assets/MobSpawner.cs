47e9a2975eae5ac21efc708c901e4bb49c368ace
riskinDesktop <hlavac.honza@email.cz>
2018-12-27 13:50:27 +0100

    Zapocata prace na MobGeneratoru

--- Santa Ninja/Assets/MobSpawner.cs
+++ Santa Ninja/Assets/MobSpawner.cs
@@ -6,15 +6,29 @@ public class MobSpawner : MonoBehaviour
 {
     public GameObject[] mobPrefabs;
     public GameObject[] mobs;
+    public Vector2[] spawnCoordinates;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnCoordinates = new Vector2[10];
         mobs = new GameObject[10]; //makes sure they match length
         for (int i = 0; i < mobs.Length; i++)
         {
             int rand = Random.Range(0, mobPrefabs.Length);
             mobs[i] = Instantiate(mobPrefabs[rand]) as GameObject;
+
+        }
+
+    }
+
+    public void SpawnMobs()
+    {
+        for (int i = 0; i < mobs.Length; i++)
+        {
+            int rand = Random.Range(0, mobPrefabs.Length);
+            mobs[i] = Instantiate(mobPrefabs[rand]) as GameObject;
+            mobs[i].transform.position = spawnCoordinates[i];
         }
     }
 
@@ -22,5 +36,5 @@ public class MobSpawner : MonoBehaviour
     void Update()
     {
         
-    }
+    }  
 }

ffcbffd8096b1c9b9aaea0ac5dfb6daf2b575924
riskinDesktop <hlavac.honza@email.cz>
2018-12-28 19:38:35 +0100

    MobSpawner takrka hotov, nefunguje jenom spawn mimo objekty

--- Santa Ninja/Assets/MobSpawner.cs
+++ Santa Ninja/Assets/MobSpawner.cs
@@ -6,13 +6,20 @@ public class MobSpawner : MonoBehaviour
 {
     public GameObject[] mobPrefabs;
     public GameObject[] mobs;
-    public Vector2[] spawnCoordinates;
+    public List<Vector2> spawnCoordinates = new List<Vector2>();
+    bool JeCasNaMoby = true;
+    public int pocetMobu = 6;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnCoordinates = new Vector2[10];
-        mobs = new GameObject[10]; //makes sure they match length
+        GenerateCoordinates();
+       /* for (int i = 0; i < spawnCoordinates.Length; i++)
+        {
+            spawnCoordinates[i] = new Vector2(40f, 40f);
+
+        }*/
+        mobs = new GameObject[pocetMobu]; //makes sure they match length
         for (int i = 0; i < mobs.Length; i++)
         {
             int rand = Random.Range(0, mobPrefabs.Length);
@@ -21,20 +28,56 @@ public class MobSpawner : MonoBehaviour
         }
 
     }
-
-    public void SpawnMobs()
+  
+    void GenerateCoordinates()
     {
-        for (int i = 0; i < mobs.Length; i++)
+        for (int i = 0; i < pocetMobu; i++)
         {
-            int rand = Random.Range(0, mobPrefabs.Length);
-            mobs[i] = Instantiate(mobPrefabs[rand]) as GameObject;
-            mobs[i].transform.position = spawnCoordinates[i];
+            while (true)
+            {
+                int a = Random.Range(2, 39);
+                int b = Random.Range(2, 39);
+                Vector2 coordinate = new Vector2(a, b);
+                if(IsSuitable(coordinate, spawnCoordinates))
+                {
+                    spawnCoordinates.Add(coordinate);
+                    break;
+                }
+            }
+            
         }
+    }
+
+    bool IsSuitable(Vector2 coordinate, List<Vector2> usedCoordinates)
+    {
+        for (int i = 0; i < usedCoordinates.Count; i++)
+        {
+            if ((Mathf.Abs(coordinate.x - usedCoordinates[i].x) > 3 && Mathf.Abs(coordinate.y - usedCoordinates[i].y) > 3)
+                && !Physics.Raycast(coordinate, Vector3.up, 2)
+                && !Physics.Raycast(coordinate, Vector3.down, 2)
+                && !Physics.Raycast(coordinate, Vector3.left, 2)
+                && !Physics.Raycast(coordinate, Vector3.right, 2))
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (JeCasNaMoby)
+        {
+            for (int i = 0; i < mobs.Length; i++)
+            {
+                mobs[i].transform.position = spawnCoordinates[i];
+            }
+            JeCasNaMoby = false;
+        }
     }  
 }

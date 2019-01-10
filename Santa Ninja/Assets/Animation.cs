a6738e8ae745334698509443bd0b863312848fa5
riskinDesktop <hlavac.honza@email.cz>
2019-01-10 21:45:12 +0100

    Animace pohybu Santy

---
+++ Santa Ninja/Assets/Animation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D santaRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        santaRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rychlost = santaRigidBody.velocity.magnitude / 4.2f;
+
+        animator.SetFloat("Rychlost", rychlost);
+    }
+}

0cc4687ba3a2aaf15d171f6eece519213670abd4
Morcinus <30431494+Morcinus@users.noreply.github.com>
2019-01-11 19:47:59 +0100

    Added NavMesh

---
+++ Santa Ninja/Assets/Scripts/Mobs/FOVRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVRotation : MonoBehaviour
+{
+	Quaternion rotation;
+
+	void Awake()
+	{
+		rotation = transform.rotation;
+	}
+	void LateUpdate()
+	{
+		transform.rotation = rotation * Quaternion.Euler(1, 1, -90);
+		Debug.Log(rotation);
+	}
+}

cfc812bf162cbe43ac58a4d93f0ddc94d5caa577
riskinDesktop <hlavac.honza@email.cz>
2019-01-19 15:11:00 +0100

    Pridana muzika, boosty kompletne fungujou i se zvukem

--- Santa Ninja/Assets/Boost.cs
+++ Santa Ninja/Assets/Boost.cs
@@ -6,49 +6,31 @@ using UnityEngine.UI;
 
 public class Boost : MonoBehaviour
 {
-	public int boostTimeLeft = 0;
-	//public Text boostCountdown;
-	public bool boostCountdownStart = true;
-	public bool boostCountdownFirst = true;
+    BoostManager boostManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boostManager = FindObjectOfType<BoostManager>();
+    }
 
-	// Start is called before the first frame update
-	void Start()
-	{
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		if (boostCountdownStart == true & boostCountdownFirst == true)
-		{
-			boostCountdownFirst = false;
-			boostTimeLeft = 10;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player" && !boostManager.isBoosted)
+        {
+            PlayerMovement santaPlayerMovement = FindObjectOfType<PlayerMovement>();
+            santaPlayerMovement.movementSpeed *= 2;
+            BoostManager boostManager = FindObjectOfType<BoostManager>();
+            boostManager.isBoosted = true;
+            SoundControls soundControls = FindObjectOfType<SoundControls>();
+            soundControls.timeToPlay = true;
+            BoostTimer boostTimer = FindObjectOfType<BoostTimer>();
+            boostTimer.timeToCountdown = true;
+            Destroy(gameObject);
 		}
-		//boostCountdown.text = ("" + boostTimeLeft); - nemame pocitadlo zatim
-	}
-
-	IEnumerator LoseTime()
-	{
-		while (boostTimeLeft > 0)
-		{
-			yield return new WaitForSeconds(1);
-			boostTimeLeft--;
-		}
-	}
-
-	private void OnTriggerEnter(Collider collision)
-	{
-		if (collision.tag == "Player")
-		{
-			boostCountdownStart = true;
-			if (boostTimeLeft > 0)
-			{
-				PlayerMovement santaPlayerMovement = FindObjectOfType<PlayerMovement>();
-				santaPlayerMovement.movementSpeed = 5;
-				Debug.Log("Given boost");
-				Destroy(gameObject);
-			}
-		}
-	}
+    }
 }
---
+++ Santa Ninja/Assets/SoundControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControls : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public bool timeToPlay = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeToPlay)
+        {
+            audioSource.Play(0);
+            timeToPlay = false;
+        }
+    }
+}

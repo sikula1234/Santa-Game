c3fe3edd241dbc4c34a42c0685704054fa5370e6
Morcinus <30431494+Morcinus@users.noreply.github.com>
2019-01-25 21:54:42 +0100

    Fixed Slow LevelGen

--- Santa Ninja/Assets/Scripts/Player/Santa.cs
+++ Santa Ninja/Assets/Scripts/Player/Santa.cs
@@ -7,8 +7,12 @@ public class Santa : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		sceneTransitions = FindObjectOfType<SceneTransitions>();
+		Camera camera = FindObjectOfType<Camera>();
+
+		camera.transform.position = new Vector3(transform.position.x, 50f ,transform.position.z);
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		

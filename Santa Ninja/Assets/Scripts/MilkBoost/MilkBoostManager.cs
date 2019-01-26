8c8a4bab822b44f200d23df150a6d955cf2ee573
Marie <m.skola>
2019-01-26 12:23:51 +0100

    milkBoost, only waiting for picture

---
+++ Santa Ninja/Assets/Scripts/MilkBoost/MilkBoostManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkBoostManager : MonoBehaviour
+{
+    public bool isMilkBoosted = false;
+    public GameObject milkBoostUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMilkBoosted)
+        {
+            milkBoostUI.SetActive(true);
+        }
+        else
+        {
+            milkBoostUI.SetActive(false);
+        }
+    }
+}
